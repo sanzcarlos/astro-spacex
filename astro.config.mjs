1c7a7a06ec254e54ebd0e54cdbd9a57c3d02a0af
carlos.sanzpenas <carlos.sanzpenas@gmail.com>
2024-05-04 02:33:03 +0000

    Disable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server"
+  integrations: [tailwind(), react()]
+  // output: "server"
   // adapter: vercel()
 });
