69e0dd2b144a90f759f04519553ddb45440ef0a8
carlos.sanzpenas <carlos.sanzpenas@gmail.com>
2024-05-04 02:13:29 +0000

    chore: 🎉 Upgrade all project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
-
 // import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
